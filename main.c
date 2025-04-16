55cf0bb40bbf8d9fb2437190403e3b4db0964303
Huseen <136820327+hmunye@users.noreply.github.com>
2025-04-16 18:21:59 -0400

    implemented buffered 8-byte stream output

--- main.c
+++ main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define BUF_SIZE 8
 
 int main(void) {
-    printf("hello, world\n");
-    return 0;
+    int ret;
+    FILE *file = fopen("messages.txt", "r");
+    if (!file) {
+        perror("fopen");
+        exit(1);
+    }
+
+    char buf[BUF_SIZE + 1];
+    size_t bytes_read;
+    while ((bytes_read = fread(buf, sizeof(*buf), BUF_SIZE, file)) > 0) {
+        buf[bytes_read] = '\0';
+        printf("read: %s\n", buf);
+    }
+
+    if (feof(file)) {
+        ret = 0;
+        goto cleanup;
+    }
+
+    if (ferror(file)) {
+        fprintf(stderr, "ERROR: failed to read from file");
+        ret = 1;
+        goto cleanup;
+    }
+
+cleanup:
+    fclose(file);
+    return ret;
 }
